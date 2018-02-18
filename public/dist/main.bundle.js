023361ca8c39d5f504aa6a4a24b51c3d381a942b
efournier92 <efournier92@gmail.com>
2018-02-18 14:16:58 -0500

    make app run

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -70,14 +70,14 @@
 "use strict";
 
 
-var _mysticController = __webpack_require__(1);
+var _levenDistController = __webpack_require__(1);
 
-angular.module('numberMystic', ['ngRoute']).config(['$routeProvider', config]).controller('mysticCtrl', _mysticController.mysticCtrl);
+angular.module('levenDist', ['ngRoute']).config(['$routeProvider', config]).controller('levenDistCtrl', _levenDistController.levenDistCtrl);
 
 function config($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: 'mystic.view.html',
-    controller: 'mysticCtrl'
+    templateUrl: 'levenDist.view.html',
+    controller: 'levenDist'
   });
 }
 
@@ -91,121 +91,9 @@ function config($routeProvider) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var levenDistCtrl = function levenDistCtrl($scope) {};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var mysticCtrl = function mysticCtrl($scope) {
-  var Range = function Range(min, max) {
-    _classCallCheck(this, Range);
-
-    this.min = min;
-    this.max = max;
-  };
-
-  function findMiddleNumber(range) {
-    return Math.floor((range.max + range.min) / 2);
-  }
-
-  $scope.start = function () {
-    $scope.step = 'getRangeInput';
-    $scope.knownRange = new Range(0, 100);
-    $scope.askingSingleNumber = false;
-    $scope.upperOfThree = undefined;
-    $scope.questionCount = 0;
-  };
-
-  $scope.start();
-
-  $scope.calcMaxQuestions = function () {
-    // 1 + Floor(log2( n ))
-    // find the number range
-    var range = $scope.knownRange.max - $scope.knownRange.min;
-    // calculate log base 2 of the range
-    var numLog = Math.log2(range);
-    // round down to nearest integer & add 1
-    $scope.maxQuestions = Math.floor(numLog) + 1;
-    $scope.step = 'chooseNumber';
-  };
-
-  function checkSingleNumber() {}
-
-  $scope.getNewRange = function (isWithinRange) {
-    $scope.questionCount += 1;
-
-    function showFinalAnswer() {
-      $scope.step = 'showFinalAnswer';
-      $scope.questionCount -= 1;
-    }
-
-    if ($scope.knownRange.max === $scope.knownRange.min) {
-      $scope.finalAnswer = $scope.askRange.max;
-      showFinalAnswer();
-      return;
-    }
-
-    if ($scope.askingSingleNumber) {
-      if (isWithinRange) {
-        $scope.finalAnswer = $scope.askRange.min;
-      } else {
-        $scope.finalAnswer = $scope.askRange.max;
-      }
-      showFinalAnswer();
-      return;
-    }
-
-    if ($scope.upperOfThree) {
-      if (isWithinRange) {
-        $scope.askingSingleNumber = true;
-      } else {
-        $scope.finalAnswer = $scope.upperOfThree;
-        showFinalAnswer();
-      }
-    }
-
-    function adjustAskRange() {
-      var middleNumber = findMiddleNumber($scope.knownRange);
-      $scope.askRange = new Range($scope.knownRange.min, middleNumber);
-    }
-
-    // perform binary search procedure
-    if (isWithinRange === undefined) {
-      // start asking questions
-      $scope.step = 'askQuestions';
-      adjustAskRange();
-    } else if (isWithinRange) {
-      // user inputed TRUE
-      // his/her number is within askRange
-      $scope.knownRange = $scope.askRange;
-      adjustAskRange();
-    } else if (!isWithinRange && !$scope.askingSingleNumber) {
-      // user inputed FALSE
-      // his/her number is outside askRange
-      $scope.knownRange.min = $scope.askRange.max + 1;
-      adjustAskRange();
-    }
-
-    // narrow down final numbers
-    if ($scope.knownRange.max === $scope.knownRange.min) {
-      // knownRange min & max are the same
-      // user number has been found
-      $scope.finalAnswer = $scope.askRange.max;
-      $scope.step = 'showFinalAnswer';
-    } else if ($scope.knownRange.max - $scope.knownRange.min === 2) {
-      // knownRange contains 3 potential answers
-      // adjust askRange to, at max, ask 2 more questions
-      $scope.upperOfThree = $scope.knownRange.max;
-      $scope.askRange.max = $scope.knownRange.min + 1;
-      $scope.askRange.min = $scope.knownRange.min;
-    } else if ($scope.knownRange.max - $scope.knownRange.min === 1) {
-      // knownRange contains 2 potential answers
-      // present user with a single number
-      $scope.askingSingleNumber = true;
-      $scope.askRange = $scope.knownRange;
-    }
-  };
-};
-
-exports.mysticCtrl = mysticCtrl;
+exports.levenDistCtrl = levenDistCtrl;
 
 /***/ })
 /******/ ]);
