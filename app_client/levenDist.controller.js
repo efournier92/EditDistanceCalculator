76b5be5d40a04be5d335d5c4b9b6e94b5048e461
efournier92 <efournier92@gmail.com>
2018-02-25 15:43:05 -0500

    add more logic to calcLevenDist function

--- app_client/levenDist.controller.js
+++ app_client/levenDist.controller.js
@@ -1,14 +1,19 @@
 const levenDistCtrl = function levenDistCtrl($scope) {
 
-  $scope.calcLevenDist = (string1, string2) => {
+  function calcLevenDist(string1, string2) {
     const lenString1 = string1.length;
     const lenString2 = string2.length;
     let t = [];
 
-    for (j = 0; j <= n; j++) { 
+    for (let j = 0; j <= lenString2; j++) { 
       t[j] = j; 
-      debugger;
     }
+
+    for (let i = 1; i <= lenString1; i++) {
+      for (let u = [i], let j = 1; j <= lenString2; j++) {
+        u[j] = string1[i - 1] === string2[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+      } t = u;
+    } return u[lenString2];
 
   }
 
