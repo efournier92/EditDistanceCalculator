49f1c0032841cef5f57ae5507b77d7e4c3bc90fe
Eric Fournier <efournier92@gmail.com>
2018-02-28 10:45:21 -0500

    update functional logic

--- app_client/levenDist.controller.js
+++ app_client/levenDist.controller.js
@@ -7,7 +7,7 @@ const levenDistCtrl = function levenDistCtrl($scope) {
     for (j = 0; j <= lenString2; j++) { 
       t[j] = j; 
     }
-    let i, u, j;
+    let i, u;
     for (i = 1; i <= lenString1; i++) {
       for (u = [i], j = 1; j <= lenString2; j++) {
         u[j] = string1[i - 1] === string2[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
