060be984587b5c975aecdd2635c8bb826ea05084
Eric Fournier <efournier92@gmail.com>
2018-03-13 16:18:30 -0400

    make spell check function work

--- app_client/levenDist.controller.js
+++ app_client/levenDist.controller.js
@@ -1,7 +1,50 @@
-import { englishWords } from './dictionaries/englishWords.js';
+import { englishWords } from './dictionaries/words.js';
 
 const levenDistCtrl = function levenDistCtrl($scope) {
   
+  $scope.englishWords = englishWords;
+  
+  $scope.calcLevenDistFly = (str1, str2) => {
+    // reset count variables
+    let j, i;
+    // reset edit distance arrays
+    let string2LenArr = [], finArr;
+    // update string lengths
+    // $scope.string1 = $scope.string1.length ? $scope.string1 : ``;
+    // $scope.string2 = $scope.string2.length ? $scope.string2 : ``;
+    let lenString1 = str1.length;
+    let lenString2 = str2.length;
+    for (j = 0; j <= lenString2; j++) { 
+      string2LenArr[j] = j; 
+    }
+    for (i = 1; i <= lenString1; i++) {
+      for (finArr = [i], j = 1; j <= lenString2; j++) {
+        finArr[j] = 
+          str1[i - 1] === str2[j - 1] 
+          ? string2LenArr[j - 1] 
+          : Math.min(string2LenArr[j - 1], string2LenArr[j], finArr[j - 1]) + 1;
+      }
+      string2LenArr = finArr;
+    }
+    return finArr[lenString2] || 0;
+  }
+
+
+  $scope.spellCheck = (wordToCheck) => {
+    let i;
+    let bestMatchDist = 100000;
+    let bestMatch = ``;
+    for (i = 0; i <= ($scope.englishWords.length - 1); i++) {
+      let wordDist = $scope.calcLevenDistFly(wordToCheck, $scope.englishWords[i]);
+      if (wordDist < bestMatchDist) {
+        bestMatch = $scope.englishWords[i];
+        bestMatchDist = wordDist;
+      }
+    }
+    console.log(`Best Match: `, bestMatch)
+    return bestMatch;
+  }
+
   // initialize default values
   $scope.string1 = ``;
   $scope.string2 = ``;
@@ -33,42 +76,9 @@ const levenDistCtrl = function levenDistCtrl($scope) {
     $scope.levenDist = finArr[lenString2] || 0;
   }
 
-  const calcLevenDist = (str1, str2) => {
-    // reset count variables
-    let j, i;
-    // reset edit distance arrays
-    let string2LenArr = [], finArr;
-    // update string lengths
-    // $scope.string1 = $scope.string1.length ? $scope.string1 : ``;
-    // $scope.string2 = $scope.string2.length ? $scope.string2 : ``;
-    let lenString1 = str1.length;
-    let lenString2 = str2.length;
-    for (j = 0; j <= lenString2; j++) { 
-      string2LenArr[j] = j; 
-    }
-    for (i = 1; i <= lenString1; i++) {
-      for (finArr = [i], j = 1; j <= lenString2; j++) {
-        finArr[j] = 
-          $scope.string1[i - 1] === $scope.string2[j - 1] 
-          ? string2LenArr[j - 1] 
-          : Math.min(string2LenArr[j - 1], string2LenArr[j], finArr[j - 1]) + 1;
-      }
-      string2LenArr = finArr;
-    }
-    return finArr[lenString2] || 0;
-  }
 
+  $scope.spellCheck(`power`);
 };
-
-const spellCheck = (wordToCheck) => {
-  let i;
-  let bestMatchDist = 100000;
-  let bestMatch = ``;
-  for (i = 0; i <= englishWords.length; i++) {
-    let wordDist = calcLevenDist(wordToCheck, englishWords[i]);
-    if (wordDist < bestMatchDist) bestMatch = englishWords[i]);
-  }
-}
 
 export { levenDistCtrl }
 
--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -91,11 +91,56 @@ function config($routeProvider) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.levenDistCtrl = undefined;
+
+var _words = __webpack_require__(2);
+
 var levenDistCtrl = function levenDistCtrl($scope) {
 
+  $scope.englishWords = _words.englishWords;
+
+  $scope.calcLevenDistFly = function (str1, str2) {
+    // reset count variables
+    var j = void 0,
+        i = void 0;
+    // reset edit distance arrays
+    var string2LenArr = [],
+        finArr = void 0;
+    // update string lengths
+    // $scope.string1 = $scope.string1.length ? $scope.string1 : ``;
+    // $scope.string2 = $scope.string2.length ? $scope.string2 : ``;
+    var lenString1 = str1.length;
+    var lenString2 = str2.length;
+    for (j = 0; j <= lenString2; j++) {
+      string2LenArr[j] = j;
+    }
+    for (i = 1; i <= lenString1; i++) {
+      for (finArr = [i], j = 1; j <= lenString2; j++) {
+        finArr[j] = str1[i - 1] === str2[j - 1] ? string2LenArr[j - 1] : Math.min(string2LenArr[j - 1], string2LenArr[j], finArr[j - 1]) + 1;
+      }
+      string2LenArr = finArr;
+    }
+    return finArr[lenString2] || 0;
+  };
+
+  $scope.spellCheck = function (wordToCheck) {
+    var i = void 0;
+    var bestMatchDist = 100000;
+    var bestMatch = '';
+    for (i = 0; i <= $scope.englishWords.length - 1; i++) {
+      var wordDist = $scope.calcLevenDistFly(wordToCheck, $scope.englishWords[i]);
+      if (wordDist < bestMatchDist) {
+        bestMatch = $scope.englishWords[i];
+        bestMatchDist = wordDist;
+      }
+    }
+    console.log('Best Match: ', bestMatch);
+    return bestMatch;
+  };
+
   // initialize default values
-  $scope.string1 = "";
-  $scope.string2 = "";
+  $scope.string1 = '';
+  $scope.string2 = '';
   $scope.levenDist = 0;
 
   // recalculate each time either string changes
@@ -107,8 +152,8 @@ var levenDistCtrl = function levenDistCtrl($scope) {
     var string2LenArr = [],
         finArr = void 0;
     // update string lengths
-    $scope.string1 = $scope.string1.length ? $scope.string1 : "";
-    $scope.string2 = $scope.string2.length ? $scope.string2 : "";
+    $scope.string1 = $scope.string1.length ? $scope.string1 : '';
+    $scope.string2 = $scope.string2.length ? $scope.string2 : '';
     var lenString1 = $scope.string1.length;
     var lenString2 = $scope.string2.length;
     for (j = 0; j <= lenString2; j++) {
@@ -122,9 +167,25 @@ var levenDistCtrl = function levenDistCtrl($scope) {
     }
     $scope.levenDist = finArr[lenString2] || 0;
   };
+
+  $scope.spellCheck('power');
 };
 
 exports.levenDistCtrl = levenDistCtrl;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var englishWords = ["the", "of", "to", "and", "a", "in", "is", "it", "you", "that", "he", "was", "for", "on", "are", "with", "as", "I", "his", "they", "be", "at", "one", "have", "this", "from", "or", "had", "by", "hot", "word", "but", "what", "some", "we", "can", "out", "other", "were", "all", "there", "when", "up", "use", "your", "how", "said", "an", "each", "she", "which", "do", "their", "time", "if", "will", "way", "about", "many", "then", "them", "write", "would", "like", "so", "these", "her", "long", "make", "thing", "see", "him", "two", "has", "look", "more", "day", "could", "go", "come", "did", "number", "sound", "no", "most", "people", "my", "over", "know", "water", "than", "call", "first", "who", "may", "down", "side", "been", "now", "find", "any", "new", "work", "part", "take", "get", "place", "made", "live", "where", "after", "back", "little", "only", "round", "man", "year", "came", "show", "every", "good", "me", "give", "our", "under", "name", "very", "through", "just", "form", "sentence", "great", "think", "say", "help", "low", "line", "differ", "turn", "cause", "much", "mean", "before", "move", "right", "boy", "old", "too", "same", "tell", "does", "set", "three", "want", "air", "well", "also", "play", "small", "end", "put", "home", "read", "hand", "port", "large", "spell", "add", "even", "land", "here", "must", "big", "high", "such", "follow", "act", "why", "ask", "men", "change", "went", "light", "kind", "off", "need", "house", "picture", "try", "us", "again", "animal", "point", "mother", "world", "near", "build", "self", "earth", "father", "head", "stand", "own", "page", "should", "country", "found", "answer", "school", "grow", "study", "still", "learn", "plant", "cover", "food", "sun", "four", "between", "state", "keep", "eye", "never", "last", "let", "thought", "city", "tree", "cross", "farm", "hard", "start", "might", "story", "saw", "far", "sea", "draw", "left", "late", "run", "don't", "while", "press", "close", "night", "real", "life", "few", "north", "open", "seem", "together", "next", "white", "children", "begin", "got", "walk", "example", "ease", "paper", "group", "always", "music", "those", "both", "mark", "often", "letter", "until", "mile", "river", "car", "feet", "care", "second", "book", "carry", "took", "science", "eat", "room", "friend", "began", "idea", "fish", "mountain", "stop", "once", "base", "hear", "horse", "cut", "sure", "watch", "color", "face", "wood", "main", "enough", "plain", "girl", "usual", "young", "ready", "above", "ever", "red", "list", "though", "feel", "talk", "bird", "soon", "body", "dog", "family", "direct", "pose", "leave", "song", "measure", "door", "product", "black", "short", "numeral", "class", "wind", "question", "happen", "complete", "ship", "area", "half", "rock", "order", "fire", "south", "problem", "piece", "told", "knew", "pass", "since", "top", "whole", "king", "space", "heard", "best", "hour", "better", "true", "during", "hundred", "five", "remember", "step", "early", "hold", "west", "ground", "interest", "reach", "fast", "verb", "sing", "listen", "six", "table", "travel", "less", "morning", "ten", "simple", "several", "vowel", "toward", "war", "lay", "against", "pattern", "slow", "center", "love", "person", "money", "serve", "appear", "road", "map", "rain", "rule", "govern", "pull", "cold", "notice", "voice", "unit", "power", "town", "fine", "certain", "fly", "fall", "lead", "cry", "dark", "machine", "note", "wait", "plan", "figure", "star", "box", "noun", "field", "rest", "correct", "able", "pound", "done", "beauty", "drive", "stood", "contain", "front", "teach", "week", "final", "gave", "green", "oh", "quick", "develop", "ocean", "warm", "free", "minute", "strong", "special", "mind", "behind", "clear", "tail", "produce", "fact", "street", "inch", "multiply", "nothing", "course", "stay", "wheel", "full", "force", "blue", "object", "decide", "surface", "deep", "moon", "island", "foot", "system", "busy", "test", "record", "boat", "common", "gold", "possible", "plane", "stead", "dry", "wonder", "laugh", "thousand", "ago", "ran", "check", "game", "shape", "equate", "hot", "miss", "brought", "heat", "snow", "tire", "bring", "yes", "distant", "fill", "east", "paint", "language", "among", "grand", "ball", "yet", "wave", "drop", "heart", "am", "present", "heavy", "dance", "engine", "position", "arm", "wide", "sail", "material", "size", "vary", "settle", "speak", "weight", "general", "ice", "matter", "circle", "pair", "include", "divide", "syllable", "felt", "perhaps", "pick", "sudden", "count", "square", "reason", "length", "represent", "art", "subject", "region", "energy", "hunt", "probable", "bed", "brother", "egg", "ride", "cell", "believe", "fraction", "forest", "sit", "race", "window", "store", "summer", "train", "sleep", "prove", "lone", "leg", "exercise", "wall", "catch", "mount", "wish", "sky", "board", "joy", "winter", "sat", "written", "wild", "instrument", "kept", "glass", "grass", "cow", "job", "edge", "sign", "visit", "past", "soft", "fun", "bright", "gas", "weather", "month", "million", "bear", "finish", "happy", "hope", "flower", "clothe", "strange", "gone", "jump", "baby", "eight", "village", "meet", "root", "buy", "raise", "solve", "metal", "whether", "push", "seven", "paragraph", "third", "shall", "held", "hair", "describe", "cook", "floor", "either", "result", "burn", "hill", "safe", "cat", "century", "consider", "type", "law", "bit", "coast", "copy", "phrase", "silent", "tall", "sand", "soil", "roll", "temperature", "finger", "industry", "value", "fight", "lie", "beat", "excite", "natural", "view", "sense", "ear", "else", "quite", "broke", "case", "middle", "kill", "son", "lake", "moment", "scale", "loud", "spring", "observe", "child", "straight", "consonant", "nation", "dictionary", "milk", "speed", "method", "organ", "pay", "age", "section", "dress", "cloud", "surprise", "quiet", "stone", "tiny", "climb", "cool", "design", "poor", "lot", "experiment", "bottom", "key", "iron", "single", "stick", "flat", "twenty", "skin", "smile", "crease", "hole", "trade", "melody", "trip", "office", "receive", "row", "mouth", "exact", "symbol", "die", "least", "trouble", "shout", "except", "wrote", "seed", "tone", "join", "suggest", "clean", "break", "lady", "yard", "rise", "bad", "blow", "oil", "blood", "touch", "grew", "cent", "mix", "team", "wire", "cost", "lost", "brown", "wear", "garden", "equal", "sent", "choose", "fell", "fit", "flow", "fair", "bank", "collect", "save", "control", "decimal", "gentle", "woman", "captain", "practice", "separate", "difficult", "doctor", "please", "protect", "noon", "whose", "locate", "ring", "character", "insect", "caught", "period", "indicate", "radio", "spoke", "atom", "human", "history", "effect", "electric", "expect", "crop", "modern", "element", "hit", "student", "corner", "party", "supply", "bone", "rail", "imagine", "provide", "agree", "thus", "capital", "won't", "chair", "danger", "fruit", "rich", "thick", "soldier", "process", "operate", "guess", "necessary", "sharp", "wing", "create", "neighbor", "wash", "bat", "rather", "crowd", "corn", "compare", "poem", "string", "bell", "depend", "meat", "rub", "tube", "famous", "dollar", "stream", "fear", "sight", "thin", "triangle", "planet", "hurry", "chief", "colony", "clock", "mine", "tie", "enter", "major", "fresh", "search", "send", "yellow", "gun", "allow", "print", "dead", "spot", "desert", "suit", "current", "lift", "rose", "continue", "block", "chart", "hat", "sell", "success", "company", "subtract", "event", "particular", "deal", "swim", "term", "opposite", "wife", "shoe", "shoulder", "spread", "arrange", "camp", "invent", "cotton", "born", "determine", "quart", "nine", "truck", "noise", "level", "chance", "gather", "shop", "stretch", "throw", "shine", "property", "column", "molecule", "select", "wrong", "gray", "repeat", "require", "broad", "prepare", "salt", "nose", "plural", "anger", "claim", "continent", "oxygen", "sugar", "death", "pretty", "skill", "women", "season", "solution", "magnet", "silver", "thank", "branch", "match", "suffix", "especially", "fig", "afraid", "huge", "sister", "steel", "discuss", "forward", "similar", "guide", "experience", "score", "apple", "bought", "led", "pitch", "coat", "mass", "card", "band", "rope", "slip", "win", "dream", "evening", "condition", "feed", "tool", "total", "basic", "smell", "valley", "nor", "double", "seat", "arrive", "master", "track", "parent", "shore", "division", "sheet", "substance", "favor", "connect", "post", "spend", "chord", "fat", "glad", "original", "share", "station", "dad", "bread", "charge", "proper", "bar", "offer", "segment", "slave", "duck", "instant", "market", "degree", "populate", "chick", "dear", "enemy", "reply", "drink", "occur", "support", "speech", "nature", "range", "steam", "motion", "path", "liquid", "log", "meant", "quotient", "teeth", "shell", "neck"];
+
+exports.englishWords = englishWords;
 
 /***/ })
 /******/ ]);
