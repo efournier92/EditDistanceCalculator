73f44bf31a0d46232f49e5e0238b087afe77272e
Eric Fournier <efournier92@gmail.com>
2018-03-16 14:01:42 -0400

    start making dynamic vertical tabs work

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -170,6 +170,18 @@ var levenDistCtrl = function levenDistCtrl($scope) {
   };
 
   $scope.spellCheck('power');
+
+  $scope.tabs = [{ title: 'Dynamic Title 1', content: 'Dynamic content 1' }, { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }];
+
+  $scope.alertMe = function () {
+    setTimeout(function () {
+      $window.alert('You\'ve selected the alert tab!');
+    });
+  };
+
+  $scope.model = {
+    name: 'Tabs'
+  };
 };
 
 exports.levenDistCtrl = levenDistCtrl;
